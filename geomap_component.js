a0f5d5b73d193d21f349e459a8eedea6d7b76036
sejam <49522774+sejam@users.noreply.github.com>
2022-03-03 10:21:59 +0100

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -30,7 +30,7 @@
     //
     // A definition query filters what was first retrieved from the SPL feature service
     function applyDefinitionQuery() {
-        var svcLyr = gMyWebmap.findLayerById( 'NapervilleElectric_MIL1_6627' ); 
+        var svcLyr = gMyWebmap.findLayerById( 'LAN_ew_20' ); 
         console.log( "Layer is");
         console.log( svcLyr);
 
