a845e24af8d4a4de160d4c976bfced217f2c61ea
sejam <49522774+sejam@users.noreply.github.com>
2022-03-10 14:49:25 +0100

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -30,7 +30,7 @@
     //
     // A definition query filters what was first retrieved from the SPL feature service
     function applyDefinitionQuery() {
-        var svcLyr = gMyWebmap.findLayerById( 'Verkehrsunfälle__2018_3432' ); 
+        var svcLyr = gMyWebmap.findLayerById( 'LAN_ew_20_9780' ); 
         console.log( "Layer is");
         console.log( svcLyr);
 
@@ -40,7 +40,7 @@
         // only execute when the sublayer is loaded. Note this is asynchronous
         // so it may be skipped over during execution and be executed after exiting this function
         svcLyr.when(function() {
-            gMyLyr = svcLyr.findSublayerById(6);    // store in global variable
+            gMyLyr = svcLyr.findSublayerById(15);    // store in global variable
             console.log("Sublayer loaded...");
             console.log( "Sublayer is");
             console.log( gMyLyr);
@@ -60,21 +60,21 @@
         if (gPassedServiceType < 1) { // display all service locations
             gMyLyr.definitionExpression = "1 = 1"
         } else if (gPassedServiceType === 1) { // display GoS = 1
-            gMyLyr.definitionExpression = "OBJECTID = '1'";
+            gMyLyr.definitionExpression = "ags = '01'";
         } else if (gPassedServiceType === 2) { // display GoS = 1
-            gMyLyr.definitionExpression = "'OBJECTID' = '2'";
+            gMyLyr.definitionExpression = "gen = 'Berlin'";
         } else if (gPassedServiceType === 3) { // display GoS = 2
-            gMyLyr.definitionExpression = "OBJECTID = '3'";
+            gMyLyr.definitionExpression = "ags = '03'";
         } else if (gPassedServiceType === 4) { // display GoS = 3
-            gMyLyr.definitionExpression = "OBJECTID = '1'";
+            gMyLyr.definitionExpression = "gen = 'Sachsen'";
         } else if (gPassedServiceType === 5) { // display GoS = 4
-            gMyLyr.definitionExpression = "Bundesland = '01'";
+            gMyLyr.definitionExpression = "bez = 'Land'";
         } else if (gPassedServiceType === 6) { // display GoS = 5
-            gMyLyr.definitionExpression = "'Bundesland' = '02'";
+            gMyLyr.definitionExpression = "bez = 'Land'";
         } else if (gPassedServiceType === 7) { // display GoS = 6
-            gMyLyr.definitionExpression = "Kreis = '03'";
+            gMyLyr.definitionExpression = "bez = 'Land'";
         } else {
-            gMyLyr.definitionExpression = "OBJECTID = '2'";
+            gMyLyr.definitionExpression = "bez = 'Land'";
         }
     }
 
