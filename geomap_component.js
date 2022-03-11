37f6b87c2fe2bf7daf8771b0c7d28a7644662bd6
sejam <49522774+sejam@users.noreply.github.com>
2022-03-11 09:13:32 +0100

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -30,7 +30,7 @@
     //
     // A definition query filters what was first retrieved from the SPL feature service
     function applyDefinitionQuery() {
-        var svcLyr = gMyWebmap.findLayerById( '17f749ca40c-layer-78' ); 
+        var svcLyr = gMyWebmap.findLayerById( '17f7439fe5f-layer-21' ); 
         console.log( "Layer is");
         console.log( svcLyr);
 
@@ -40,7 +40,7 @@
         // only execute when the sublayer is loaded. Note this is asynchronous
         // so it may be skipped over during execution and be executed after exiting this function
         svcLyr.when(function() {
-            gMyLyr = svcLyr.findLayerById(17);    // store in global variable
+            gMyLyr = svcLyr.findLayerById(2);    // store in global variable
             console.log("Sublayer loaded...");
             console.log( "Sublayer is");
             console.log( gMyLyr);
@@ -60,21 +60,21 @@
         if (gPassedServiceType < 1) { // display all service locations
             gMyLyr.definitionExpression = "1 = 1"
         } else if (gPassedServiceType === 1) { // display GoS = 1
-            gMyLyr.definitionExpression = "OBJECTID = '1'";
+            gMyLyr.definitionExpression = "ISO2 = 'NL'";
         } else if (gPassedServiceType === 2) { // display GoS = 1
-            gMyLyr.definitionExpression = "OBJECTID = '2'";
+            gMyLyr.definitionExpression = "ISO2 = 'BE'";
         } else if (gPassedServiceType === 3) { // display GoS = 2
-            gMyLyr.definitionExpression = "OBJECTID = '3'";
+            gMyLyr.definitionExpression = "ISO2 = 'DE'";
         } else if (gPassedServiceType === 4) { // display GoS = 3
-            gMyLyr.definitionExpression = "OBJECTID = '4'";
+            gMyLyr.definitionExpression = "ISO3 = 'NLD'";
         } else if (gPassedServiceType === 5) { // display GoS = 4
-            gMyLyr.definitionExpression = "OBJECTID = '5'";
+            gMyLyr.definitionExpression = "ISO3 = 'BEL'";
         } else if (gPassedServiceType === 6) { // display GoS = 5
-            gMyLyr.definitionExpression = "OBJECTID = '6'";
+            gMyLyr.definitionExpression = "ISO3 = 'DEU'";
         } else if (gPassedServiceType === 7) { // display GoS = 6
-            gMyLyr.definitionExpression = "OBJECTID = '7'";
+            gMyLyr.definitionExpression = "ISO3 = 'NLD'";
         } else {
-            gMyLyr.definitionExpression = "OBJECTID = '8'";
+            gMyLyr.definitionExpression = "ISO2 = 'DE'";
         }
     }
 
