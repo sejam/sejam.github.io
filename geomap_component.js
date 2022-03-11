e902a626ec83d1b0d676a9fff4bd4c5da8ffe7e1
sejam <49522774+sejam@users.noreply.github.com>
2022-03-11 17:52:39 +0100

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -57,14 +57,23 @@
     function processDefinitionQuery()
     {
         // values of passedServiceType
-        if (gPassedServiceType === 0) { // alle länder
+        if (gPassedServiceType <= 1) { // display all service locations
             gMyLyr.definitionExpression = "1 = 1"
-        } else if (gPassedServiceType <= 3) { //wenn NL ausgewählt
-            gMyLyr.definitionExpression = "ISO2 = 'DE'";    
-        } else if (gPassedServiceType === 4) { //wenn NL ausgewählt
+        } else if (gPassedServiceType === 2) { // display GoS = 1
+            gMyLyr.definitionExpression = "ISO2 = 'DE'"; 
+        } else if (gPassedServiceType === 3) { // display GoS = 2
+            gMyLyr.definitionExpression = "ISO2 = 'DE'"; 
+        } else if (gPassedServiceType === 4) { // display GoS = 3
             gMyLyr.definitionExpression = "ISO2 = 'NL'";
-        } else if (gPassedServiceType >= 5) { //wenn NL ausgewählt
-            gMyLyr.definitionExpression = "ISO2 = 'DE'";
+        } else if (gPassedServiceType === 5) { // display GoS = 4
+            gMyLyr.definitionExpression = "ISO2 = 'DE'"; 
+        } else if (gPassedServiceType === 6) { // display GoS = 5
+            gMyLyr.definitionExpression = "ISO2 = 'DE'"; 
+        } else if (gPassedServiceType === 7) { // display GoS = 6
+            gMyLyr.definitionExpression = "ISO2 = 'DE'"; 
+        } else { // default is to only display service locations with a set GoS
+            gMyLyr.definitionExpression = "NODISCONCT IN ('1', '2', '3', '4', '5', '6')";
+        }
     }
 
     class Map extends HTMLElement {
