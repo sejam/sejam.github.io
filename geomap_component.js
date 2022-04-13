5899c44d9b9812b6befc09182a4360b581cd2c2b
sejam <49522774+sejam@users.noreply.github.com>
2022-04-13 12:09:55 +0200

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -14,7 +14,7 @@
             width: 100%;
             height: 100%;
         }
-        #timeSlider {
+       /* #timeSlider {
             position: absolute;
             left: 5%;
             right: 15%;
