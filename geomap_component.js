84085ea4f610b1414d9d74d365e2c599f1375f78
sejam <49522774+sejam@users.noreply.github.com>
2022-04-19 13:35:32 +0200

    Create geomap_component.js

---
+++ geomap_component.js
@@ -0,0 +1,169 @@
+(function() {
+    let template = document.createElement("template");
+    var gPassedServiceType; // holds passed in guarantee of service - set in onCustomWidgetBeforeUpdate()
+    var gPassedPortalURL; //ESRI Portal URL
+    var gPassedAPIkey; //ESRI JS api key
+    var gWebmapInstantiated = 0; // a global used in applying definition query
+    var gMyLyr; // for sublayer
+    var gMyWebmap; // needs to be global for async call to onCustomWidgetAfterUpdate()
+
+    template.innerHTML = `
+        <link rel="stylesheet"https://js.arcgis.com/4.23/esri/themes/dark/main.css">
+        <style>
+        #mapview {
+            width: 100%;
+            height: 100%;
+        }
+        </style>
+        <div id='mapview'></div>
+    `;
+    
+    // this function takes the passed in servicelevel and issues a definition query
+    // to filter service location geometries
+    //
+    // A definition query filters what was first retrieved from the SPL feature service
+    function applyDefinitionQuery() {
+        var svcLyr = gMyWebmap.findLayerById( '180412e30b4-layer-4' ); 
+
+        // make layers visible
+        svcLyr.visible = true;
+        
+        // run the query
+         processDefinitionQuery();
+    };
+
+    // process the definition query on the passed in SPL feature sublayer
+    function processDefinitionQuery()
+    {
+        // wenn von außen entschieden werden soll welches layer angezeigt wird
+    }
+
+    class Map extends HTMLElement {
+        constructor() {
+            super();
+            
+            //this._shadowRoot = this.attachShadow({mode: "open"});
+            this.appendChild(template.content.cloneNode(true));
+            this._props = {};
+            let that = this;
+
+            require([
+                "esri/config",
+                "esri/WebMap",
+                "esri/views/MapView",
+                "esri/widgets/BasemapToggle",
+                "esri/layers/FeatureLayer",
+                "esri/widgets/Expand",
+                "esri/tasks/RouteTask",
+                "esri/tasks/support/RouteParameters",
+                "esri/tasks/support/FeatureSet",
+                "esri/layers/support/Sublayer",
+                "esri/Graphic",
+                "esri/views/ui/UI",
+                "esri/views/ui/DefaultUI" 
+            ], function(esriConfig, WebMap, MapView, BasemapToggle, FeatureLayer, Expand, RouteTask, RouteParameters, FeatureSet, Sublayer, Graphic) {
+        
+                // set portal and API Key
+                esriConfig.portalUrl = gPassedPortalURL
+
+                //  set esri api Key 
+                esriConfig.apiKey = gPassedAPIkey
+        
+                // set routing service
+                var routeTask = new RouteTask({
+                    url: "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World"
+                });
+        
+                // replace the ID below with the ID to your web map
+                const webmap = new WebMap ({
+                    portalItem: {
+                        id: "c28062a42dde47a6953e4f789a0e7eea"
+                    }
+                });
+
+                gMyWebmap = webmap;  // save to global variable
+
+                const view = new MapView({
+                    container: "mapview",
+                    map: webmap
+                });
+
+            }); // end of require()
+            
+        } // end of constructor()    
+
+        getSelection() {
+            return this._currentSelection;
+        }
+
+        onCustomWidgetBeforeUpdate(changedProperties)
+        {
+            this._props = { ...this._props, ...changedProperties };
+           // console.log(["Service Level",changedProperties["servicelevel"]]);
+
+        }
+
+        onCustomWidgetAfterUpdate(changedProperties) 
+        {
+            if ("servicelevel" in changedProperties) {
+                this.$servicelevel = changedProperties["servicelevel"];
+            }
+            gPassedServiceType = this.$servicelevel; // place passed in value into global
+
+            if ("portalurl" in changedProperties) {
+                this.$portalurl = changedProperties["portalurl"];
+            }
+            gPassedPortalURL = this.$portalurl; // place passed in value into global
+
+            if ("apikey" in changedProperties) {
+                this.$apikey = changedProperties["apikey"];
+            }
+            gPassedAPIkey = this.$apikey; // place passed in value into global
+
+            // only attempt to filter displayed service locations if the webmap is initialized
+           if (gWebmapInstantiated === 1) {
+                applyDefinitionQuery();
+            }
+        }
+    } // end of class
+
+
+
+
+    let scriptSrc = "https://js.arcgis.com/4.18/"
+    let onScriptLoaded = function() {
+        customElements.define("com-sap-custom-geomap", Map);
+    }
+
+    //SHARED FUNCTION: reuse between widgets
+    //function(src, callback) {
+    let customElementScripts = window.sessionStorage.getItem("customElementScripts") || [];
+    let scriptStatus = customElementScripts.find(function(element) {
+        return element.src == scriptSrc;
+    });
+
+    if (scriptStatus) {
+        if(scriptStatus.status == "ready") {
+            onScriptLoaded();
+        } else {
+            scriptStatus.callbacks.push(onScriptLoaded);
+        }
+    } else {
+        let scriptObject = {
+            "src": scriptSrc,
+            "status": "loading",
+            "callbacks": [onScriptLoaded]
+        }
+        customElementScripts.push(scriptObject);
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = scriptSrc;
+        script.onload = function(){
+            scriptObject.status = "ready";
+            scriptObject.callbacks.forEach((callbackFn) => callbackFn.call());
+        };
+        document.head.appendChild(script);
+    }
+
+//END SHARED FUNCTION
+})(); // end of class
