31347ed88b9df7bdda4eb92d1f4f2ec6582b9128
sejam <49522774+sejam@users.noreply.github.com>
2022-04-20 15:20:37 +0200

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -19,7 +19,7 @@
         max-width: 170px;
         max-height: 200px;
         //background-color: rgba(255, 255, 255, 0.8);
-        font-size: 1.1em;
+        font-size: 2em;
         lineHeight: 0;
         opacity: 0.9;
         boxWidth: 6;
