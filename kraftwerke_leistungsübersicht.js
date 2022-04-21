bbc6aeef7e7f4b12f195459772103e30d16f5825
sejam <49522774+sejam@users.noreply.github.com>
2022-04-21 22:36:12 +0200

    Update kraftwerke_leistungsübersicht.js

--- kraftwerke_leistungsübersicht.js
+++ kraftwerke_leistungsübersicht.js
@@ -30,7 +30,7 @@
     //
     // A definition query filters what was first retrieved from the SPL feature service
     function applyDefinitionQuery() {
-        var svcLyr = gMyWebmap.findLayerById( '1804dc03d2e-layer-2' ); 
+        var svcLyr = gMyWebmap.findLayerById( '1804b2c4eb4-layer-2' ); 
         console.log( "Layer is");
         console.log( svcLyr);
 
@@ -125,7 +125,7 @@
                 // replace the ID below with the ID to your web map
                 const webmap = new WebMap ({
                     portalItem: {
-                        id: "d3d0818051eb46d4887ae4830de70de7"
+                        id: "6e7e81e4504444b6a62486de1cd8744e"
                     }
                 });
 
