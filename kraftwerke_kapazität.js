dbea1a5c05f5fd444c072dff90bec9dfd7058a6a
sejam <49522774+sejam@users.noreply.github.com>
2022-04-22 13:22:13 +0200

    Update kraftwerke_kapazität.js

--- kraftwerke_kapazität.js
+++ kraftwerke_kapazität.js
@@ -16,7 +16,7 @@
         }
         #legendDiv {
         max-width: 200px;
-        max-height: 340px;
+        max-height: 350px;
         font-size: 1em;
         lineHeight: 0;
         opacity: 0.8;
