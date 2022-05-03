53dcf30ef03fcf71ac7e4221b7dced3ee5bd64f5
sejam <49522774+sejam@users.noreply.github.com>
2022-05-03 16:56:01 +0200

    Create Cluster_component.js

---
+++ Cluster_component.js
@@ -0,0 +1,142 @@
+(function() {
+    let template = document.createElement("template");
+    var gPassedServiceType; // holds passed in guarantee of service - set in onCustomWidgetBeforeUpdate()
+    var gPassedPortalURL; //ESRI Portal URL
+    var gPassedAPIkey; //ESRI JS api key
+    var gWebmapInstantiated = 0; // a global used in applying definition query
+    var gMyLyr; // for sublayer
+    var gMyWebmap; // needs to be global for async call to onCustomWidgetAfterUpdate()
+
+    template.innerHTML = `
+        <link rel="stylesheet" href="https://js.arcgis.com/4.23/esri/themes/light/main.css">
+        <style>
+        #mapview {
+            width: 100%;
+            height: 100%;
+        }
+        </style>
+        <div id='mapview'></div>
+    `;
+    
+    // this function takes the passed in servicelevel and issues a definition query
+    // to filter service location geometries
+    //
+    // A definition query filters what was first retrieved from the SPL feature service
+    function applyDefinitionQuery() {
+        var svcLyr = gMyWebmap.findLayerById( '1808a4d63be-layer-2' ); 
+
+        // make layers visible
+        svcLyr.visible = true; 
+    };
+
+    class Map extends HTMLElement {
+        constructor() {
+            super();
+            
+            //this._shadowRoot = this.attachShadow({mode: "open"});
+            this.appendChild(template.content.cloneNode(true));
+            this._props = {};
+            let that = this;
+
+            require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Legend", "esri/widgets/Expand"],
+                    function(esriConfig, WebMap, MapView, Legend, Expand) {
+                
+                // set portal and API Key
+                esriConfig.portalUrl = gPassedPortalURL
+
+                //  set esri api Key 
+                esriConfig.apiKey = gPassedAPIkey
+        
+                // replace the ID below with the ID to your web map
+                const webmap = new WebMap ({
+                    portalItem: {
+                        id: "621801f6f8a44b999dc02633b1ff3f7e"
+                    }
+                });
+
+                gMyWebmap = webmap;  // save to global variable
+
+                const view = new MapView({
+                    container: "mapview",
+                    map: webmap,
+                    zoom: 7
+                });
+                
+            }); // end of require()
+        } // end of constructor()    
+
+        getSelection() {
+            return this._currentSelection;
+        }
+
+        onCustomWidgetBeforeUpdate(changedProperties)
+        {
+            this._props = { ...this._props, ...changedProperties };
+           // console.log(["Service Level",changedProperties["servicelevel"]]);
+
+        }
+
+        onCustomWidgetAfterUpdate(changedProperties) 
+        {
+            if ("servicelevel" in changedProperties) {
+                this.$servicelevel = changedProperties["servicelevel"];
+            }
+            gPassedServiceType = this.$servicelevel; // place passed in value into global
+
+            if ("portalurl" in changedProperties) {
+                this.$portalurl = changedProperties["portalurl"];
+            }
+            gPassedPortalURL = this.$portalurl; // place passed in value into global
+
+            if ("apikey" in changedProperties) {
+                this.$apikey = changedProperties["apikey"];
+            }
+            gPassedAPIkey = this.$apikey; // place passed in value into global
+
+            // only attempt to filter displayed service locations if the webmap is initialized
+           if (gWebmapInstantiated === 1) {
+                applyDefinitionQuery();
+            }
+        }
+    } // end of class
+
+
+
+
+    let scriptSrc = "https://js.arcgis.com/4.18/"
+    let onScriptLoaded = function() {
+        customElements.define("com-sap-custom-geomap-1", Map);
+    }
+
+    //SHARED FUNCTION: reuse between widgets
+    //function(src, callback) {
+    let customElementScripts = window.sessionStorage.getItem("customElementScripts") || [];
+    let scriptStatus = customElementScripts.find(function(element) {
+        return element.src == scriptSrc;
+    });
+
+    if (scriptStatus) {
+        if(scriptStatus.status == "ready") {
+            onScriptLoaded();
+        } else {
+            scriptStatus.callbacks.push(onScriptLoaded);
+        }
+    } else {
+        let scriptObject = {
+            "src": scriptSrc,
+            "status": "loading",
+            "callbacks": [onScriptLoaded]
+        }
+        customElementScripts.push(scriptObject);
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = scriptSrc;
+        script.onload = function(){
+            scriptObject.status = "ready";
+            scriptObject.callbacks.forEach((callbackFn) => callbackFn.call());
+        };
+        document.head.appendChild(script);
+    }
+
+//END SHARED FUNCTION
+})(); // end of class
