dcefaeb7f1c3d65158f117672c07010e457223ae
sejam <49522774+sejam@users.noreply.github.com>
2022-05-20 10:18:57 +0200

    Update filter_component.js

--- filter_component.js
+++ filter_component.js
@@ -130,13 +130,11 @@
                 });
         
                 // replace the ID below with the ID to your web map
-                const webmap = new WebMap ({
+                /*const webmap = new WebMap ({
                     portalItem: {
                         id: "d0d1305e34ef49bc9888f590758d5128"
                     }
-                });
-
-                gMyWebmap = webmap;  // save to global variable
+                });*/
 
                 let ensourceLayerView;
                 
@@ -152,6 +150,8 @@
                     basemap: "streets-navigation-vector",
                     layers: [layer]
                 });
+                
+                gMyWebmap = map;  // save to global variable
 
                 const view = new MapView({
                     container: "mapview",
